6ff87c39f98a3f3466a1d8ca0233de80b7cfe459
Teffen Ellis (Eric) <teffen@nirri.us>
2015-03-01 01:44:42 -0800

    Flesh out entries table layout.

--- app/common/entries/index.jsx
+++ app/common/entries/index.jsx
@@ -15,20 +15,38 @@ export default React.createClass({
   },
 
   render() {
-    return <ol data-component="entries">
-      {this.renderEntries()}
-    </ol>
+    return <section data-component="entries">
+     {this.renderTable()}
+    </section>
   },
 
   renderEntries() {
     return this.props.entries.map((entry, i) =>
-      <li key={i}>
-        <span className="link"
-          onClick={this.handleNavigate.bind(this, {
-          username: this.props.username,
-          entryID: entry.id
-        })}>{entry.contentTitle}</span>
-      </li>
+      <tr key={i}>
+        <td className="entry-number">{i + 1}.</td>
+        <td className="entry">
+          <span className="link"
+            onClick={this.handleNavigate.bind(this, {
+              username: this.props.username,
+              entryID: entry.id
+            })}>
+            {entry.contentTitle}
+          </span>
+        </td>
+      </tr>
     )
+  },
+
+  renderTable() {
+    return <table>
+      <thead>
+        <th className='entry-number'>№</th>
+        <th>Entry</th>
+      </thead>
+
+      <tbody>
+        {this.renderEntries()}
+      </tbody>
+    </table>
   }
 })
